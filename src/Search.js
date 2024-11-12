f840e510968c8f71384b6aa14e9b7c53235d9974
Mehdi Hannachi <mehdihannachi92@gmail.com>
2024-11-12 20:38:41 +0100

    search and home page

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
@@ -17,14 +17,21 @@ function Search() {
         startDate: startDate,
         endDate: endDate,
         key: "selection",
-    };
+      };
 
     function handleSelect(ranges) {
         setStartDate(ranges.selection.startDate);
         setEndDate(ranges.selection.endDate);
     }
     return (
-        null
+        <div className='search'>
+            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+            <h2>
+                Number of guests <PeopleIcon />
+            </h2>
+            <input min={0} defaultValue={2} type="number" />
+            <Button onClick={() => history.push('/search')}>Search Airbnb</Button>
+        </div>
     )
 }
 
